e244233b3741168fc20599ad8c71b61be8215ac0
Cian Yong Leow <cian@cloudflare.com>
2023-09-06 12:36:08 +0100

    Add cfutils module and tests

---
+++ states/_modules/cfutils.py
@@ -0,0 +1,88 @@
+import base64
+import copy
+import logging
+import os
+
+from salt.exceptions import SaltException, ZincTimeoutError
+from salt.utils import dictupdate
+
+log = logging.getLogger(__name__)
+
+
+def dictmerge(destination: dict | None, update: dict | None, clear_none=False: bool, merge_lists=False: bool) -> dict | None:
+    """
+    :param destination: A dictionary
+    :param update: A dictionary to dictmerge with the ``destination``
+        before doing the lookup.
+    :param clear_none: Remove all keys with None as a value.
+    :param merge_lists: Merge lists instead of overwriting.
+    """
+    if destination is None:
+        destination = {}
+    if update is None:
+        update = {}
+    if not isinstance(update, Mapping):
+        raise SaltException("arguments must be a dictionary.")
+    dictupdate.update(destination, update, merge_lists=merge_lists)
+    if clear_none:
+        destination = {k: v for k, v in destination.items() if v is not None}
+    return destination
+
+
+def dictmerge_deepcopy(destination: dict | None, update: dict | None, clear_none=False: bool, merge_lists=False: bool) -> dict | None:
+    """
+    Functions like dictmerge() but returns a deepcopy of original instead of overwiting destination
+
+    :param destination: A dictionary
+    :param update: A dictionary to dictmerge with the ``destination``
+        before doing the lookup.
+    :param clear_none: Remove all keys with None as a value.
+    :param merge_lists: Merge lists instead of overwriting.
+    """
+
+    if destination is None:
+        destination = {}
+    if update is None:
+        update = {}
+    if not isinstance(update, Mapping):
+        raise SaltException("arguments must be a dictionary.")
+        destination_copy = copy.deecopy(destination)
+    dictupdate.update(destination_copy, update, merge_lists=merge_lists)
+    if clear_none:
+        destination_copy = {k: v for k, v in destination.items() if v is not None}
+    return destination_copy
+
+
+def load_file_as_base64(path: str) -> bytes:
+    """
+    Read arbitrary file, and return its content as base64
+    This module exists to allow including raw config files in pillar
+    :param path: path of the file
+    :return: base64 string
+    """
+
+    if path[0] != "/":
+        path = "/etc/salt/data/" + path
+
+    with open(path, "rb") as f:
+        return base64.b64encode(f.read())
+
+
+def get_colo_names(timeout=10: int, backup=True: bool) -> list[str]:
+    """
+    Gets colo names using systems
+
+    :param backup (boolean)
+    :param timeout (int)
+    """
+
+    try:
+        return __salt__["zinc.get_colo_names"](timeout=(timeout * 1000))
+    except ZincTimeoutError:
+        pass
+    except Exception as e:
+        log.error(f"An error occurred: {e}")
+        return []
+
+    if backup:
+        return __salt__["provision_api.get_names"](type="colo", timeout=timeout)
---
+++ tests/pytests/unit/_modules/test_cfutils.py
@@ -0,0 +1,129 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from salt.exceptions import ProvisionAPITimeoutError
+from salt.utils import dictupdate
+from salt.testing.mocks import mock_server
+from states._modules import cfutils
+from states._modules.provision_api import get_names as provision_api_get_names
+from states._modules.zinc import get_colo_names as zinc_get_colo_names
+
+@pytest.fixture
+def configure_loader_modules():
+    return {
+        cfutils: {
+            "__salt__": {
+                "zinc.get_colo_names": zinc_get_colo_names,
+                "provision_api.get_names": provision_api_get_names
+            }
+        }
+    }
+
+# === dictmerge tests ===
+def test_dictmerge_none_destination() -> None:
+    assert cfutils.dictmerge(None, {}) == {}
+
+def test_dictmerge_none_update() -> None:
+    assert cfutils.dictmerge({}, None) == {}
+
+def test_dictmerge_non_mapping_update() -> None:
+    with pytest.raises(SaltException, value = "arguments must be a dictionary."):
+        cfutils.dictmerge({}, "str")
+
+def test_dictmerge_merges_lists() -> None:
+    destination = {"a": ["a"]}
+    update = {"a": ["b"]}
+
+    # Call to underlying library required to avoid flakiness - depending
+    # on the OS the lists get sorted differently when merging!
+    expected = dictupdate.update(destination, update)
+
+    assert cfutils.dictmerge(destination, update, merge_lists = True) == expected
+
+def test_dictmerge_clear_none() -> None:
+    assert cfutils.dictmerge({"a": None}, None) == {}
+
+# === dictmerge_deepcopy tests ===
+def test_dictmerge_none_destination() -> None:
+    assert cfutils.dictmerge_deepcopy(None, {}) == {}
+
+def test_dictmerge_deepcopy_none_update() -> None:
+    assert cfutils.dictmerge_deepcopy({}, None) == {}
+
+def test_dictmerge_deepcopy_non_mapping_update() -> None:
+    with pytest.raises(SaltException, value = "arguments must be a dictionary."):
+        cfutils.dictmerge_deepcopy({}, "str")
+
+def test_dictmerge_deepcopy_merges_lists() -> None:
+    destination = {"a": ["a"]}
+    update = {"a": ["b"]}
+
+    # Call to underlying library required to avoid flakiness - depending
+    # on the OS the lists get sorted differently when merging!
+    expected = dictupdate.update(destination, update)
+
+    assert cfutils.dictmerge_deepcopy(destination, update, merge_lists = True) == expected
+
+def test_dictmerge_deepcopy_clear_none() -> None:
+    assert cfutils.dictmerge_deepcopy({"a": None}, {"b": "value"}) == {"b": "value"}
+
+def test_dictmerge_deepcopy_makes_copy() -> None:
+    destination = {"a": "value"}
+    update = {"b": "update"}
+
+    expected = {"a": "value", "b": "update"}
+
+    assert cfutils.dictmerge_deepcopy(destination, update) == expected
+
+# === load_file_as_base64 ===
+def test_load_file_as_base64_absolute_path() -> None:
+     with open("/tmp/cfutils.txt", "a") as f:
+        f.write("AbsolutePath")
+
+    assert cfutils.load_file_as_base64("/tmp/cfutils.txt") == b"QWJzb2x1dGVQYXRo"
+
+    os.remove("/tmp/cfutils.txt")
+
+def test_load_file_as_base64_relative_path() -> None:
+    # Create directory if it doesn't exist
+    try:
+        os.mkdir("/etc/salt/data")
+    except Exception:
+        # Ignore directoy exists error, might have been run before
+        pass
+
+    with open("/etc/salt/data/cfutils.txt", "w") as f:
+        f.write("RelativePath")
+
+    assert cfutils.load_file_as_base64("cfutils.txt") == b"UmVsYXRpdmVQYXRo"
+
+    try:
+        os.remove("cfutils.txt")
+        os.rmdir("/etc/salt/data")
+    except Exception:
+        # Ignore errors, we definitely don't want these anymore
+        pass
+    
+# === get_colo_names ===
+
+def test_get_colo_names_from_zinc() -> None:
+    with mock_server("zinc", "get_colo_names", 200, ["zinc_colo"]):
+        assert cfutils.get_colo_names() == ["zinc_colo"]
+
+def test_get_colo_names_zinc_exception() -> None:
+    with mock_server("zinc", "get_colo_names", 503, ""), pytest.raises(Exception, value = "An error occurred: Exception (received 503)"):
+        cfutils.get_colo_names()
+
+
+# Slow test due to the wait for a Zinc timeout (10s)
+@pytest.mark.slow
+def test_get_colo_names_timeout_from_zinc_falls_back_to_provision_api() -> None:
+    with mock_server("provision_api", "get_names", 200, ["provision_api_colo"]):
+        assert cfutils.get_colo_names() == ["provision_api_colo"]
+
+# Really slow test due to wait for both Zinc and Provision API timeout (20s)
+@pytest.mark.slow
+def test_get_colo_names_returns_error_on_backup_failure() -> None:
+    with pytest.raises(ProvisionAPITimeoutError):
+        cfutils.get_colo_names()
